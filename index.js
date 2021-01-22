c175d55391b7a56a2543804136383d1225d7c220
Noor Albayaty <noor.albayaty90@gmail.com>
2021-01-22 22:20:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,12 +12,7 @@ let arr=[];
 
 function johnLennonFacts(facts)
 {
-  const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+  const fact=[];
 var i = 0;
 while (i<facts.length) {
   fact.push(`${facts[i]} !!!`);
